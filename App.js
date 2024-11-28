86d4138e5003cbf6eb6b2c7784d85de5cc596ba9
23048180-yongkeong <23048180@myrp.edu.sg>
2024-11-29 00:18:27 +0800

    Retrieve data from datasource without navigation with working calculation.

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, TouchableOpacity, Text, FlatList, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
-export default function App() {
+const App = () => {
+  let total = 0;
+  for (let i = 0; i < datasource.length; i++) {
+      total += datasource[i].price * datasource[i].quantity;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+          style={[styles.row, {justifyContent: 'center', marginBottom: 20}]}>
+          <Icon name="plus" size={40} color='black' />
+      </TouchableOpacity>
+      <FlatList data={datasource} renderItem={renderItem} />
+        <View style={[styles.row, {marginTop: 20}]}>
+            <Text style={styles.totaltext}>Total Price</Text>
+            <Text style={styles.totaltext}>$ {total.toFixed(2)}</Text>
+        </View>
     </View>
   );
 }
 
+export default App;
+
+const renderItem = ({item}) => {
+  return (
+      <View style={[styles.row, {marginTop: 20, marginBottom: 20}]}>
+          <Text style={styles.datatext}>{item.name}</Text>
+          <Text style={styles.datatext}>{item.quantity}</Text>
+          <Text style={styles.datatext}>$ {item.price.toFixed(2)}</Text>
+            <View style={styles.column}>
+              <TouchableOpacity
+                  style={{marginBottom: 20}}>
+                  <Icon name="pen-to-square" size={35} color='orange' />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  style={{marginBottom: 20}}>
+                  <Icon name="trash-can" size={35} color='red' />
+              </TouchableOpacity>
+            </View>
+      </View>
+  );
+};
+
+const datasource= [
+    {name: "Potato Chip", quantity: 1, price: 2.90},
+    {name: "Chocolate Chip Cookies", quantity: 2, price: 5.00},
+    {name: "Thai Teas", quantity: 4, price: 1.50},
+    {name: "Lemon Teas", quantity: 3, price: 1.50}
+];
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'blue',
+        marginTop: 30
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        padding: 20,
+        backgroundColor: 'lime',
+    },
+    column: {
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+    },
+    datatext: {
+        fontSize: 20,
+        flexWrap: 'wrap',
+        width: '25%',
+        marginRight: 20
+    },
+    totaltext: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginRight: 40
+    }
 });
